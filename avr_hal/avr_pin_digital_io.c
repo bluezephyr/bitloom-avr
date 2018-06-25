a25d6b0ce3621a0a7211651fccc636bec7ff4436
Blue Zephyr <blue.zephyr.git@gmail.com>
2018-06-25 20:10:45 +0200

    Added avr_pin_digital_io.c

---
+++ avr_hal/avr_pin_digital_io.c
@@ -0,0 +1,20 @@
+/*
+ * Implementation of the PIN digital IO module for AVR.
+ *
+ * Copyright (c) 2018. BlueZephyr
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/io.h>
+
+#include "port_config.h"
+
+void pin_digital_io_write(uint16_t pin_id, bool on)
+{
+    LED_PORT |= (on << pin_id);
+}
