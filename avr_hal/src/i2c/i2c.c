665b90d68a0de08c30dce4daf86e8d150034e0cd
Blue Zephyr <blue.zephyr.git@gmail.com>
2020-12-13 16:06:51 +0100

    Implement I2C write

--- avr_hal/src/i2c/i2c.c
+++ avr_hal/src/i2c/i2c.c
@@ -11,6 +11,7 @@
 #include "hal/i2c.h"
 #include <util/twi.h>
 #include <avr/interrupt.h>
+#include <stddef.h>
 
 /*
  * The TWINT Flag is set in the following situations:
@@ -52,14 +53,18 @@ static struct i2c_controller_t
 {
     i2c_operation_t operation;
     uint8_t slave_address;
-    i2c_operation_status_t operation_status;
+    uint8_t read_register;
     i2c_state_t state;
-    uint8_t* data;
-    uint8_t data_len;
+    uint8_t* buffer;
+    uint8_t buffer_length;
     uint8_t handled_bytes;
+    enum i2c_op_result_t *operation_result;
+    uint8_t operation_status_code;
 } self;
 
-
+/*
+ * Local function declarations
+ */
 static inline void i2c_send_start (void);
 static inline void i2c_write_byte (uint8_t byte);
 static inline void i2c_send_stop (void);
@@ -69,7 +74,9 @@ void i2c_init (void)
 {
     self.state = idle;
     self.slave_address = 0;
-    self.operation_status = i2c_ok;
+    self.read_register = 0;
+    self.operation_result = NULL;
+    self.operation_status_code = 0;
 
     // TWBR – TWI Bit Rate Register
     // Datasheet page 230
@@ -77,38 +84,60 @@ void i2c_init (void)
     TWCR |= (1 << TWEN);
 }
 
-void i2c_write (uint8_t address, uint8_t *data, uint8_t data_len)
+enum i2c_request_t i2c_write (uint8_t address, uint8_t *buffer, uint8_t length, enum i2c_op_result_t *result)
 {
     if (self.state == idle)
     {
         // Store the input data
         self.slave_address = address;
-        self.data = data;
-        self.data_len = data_len;
+        self.buffer = buffer;
+        self.buffer_length = length;
 
         // Prepare write operation - will be performed in ISR context
         self.operation = write_op;
-        self.operation_status = i2c_busy;
+        self.operation_result = result;
+        *self.operation_result = i2c_operation_processing;
         self.state = start;
         self.handled_bytes = 0;
         i2c_send_start();
+
+        return i2c_request_ok;
     }
+    return i2c_request_busy;
 }
 
-void i2c_read_register (uint8_t slave_id, uint8_t reg, uint8_t *data, uint8_t data_len)
+enum i2c_request_t i2c_read_register(uint8_t address, uint8_t read_register, uint8_t *buffer,
+                                     uint8_t length, enum i2c_op_result_t *result)
 {
+    if (self.state == idle)
+    {
+        self.slave_address = address;
+        self.read_register = read_register;
+        self.buffer = buffer;
+        self.buffer_length = length;
 
+        // Prepare read register operation. Will be started next tick
+        self.operation = read_register_op;
+        self.operation_result = result;
+        *self.operation_result = i2c_operation_processing;
+        self.state = start;
+        self.handled_bytes = 0;
+        i2c_send_start();
+
+        return i2c_request_ok;
+    }
+    return i2c_request_busy;
 }
 
-i2c_operation_status_t i2c_get_operation_status(void)
+uint8_t i2c_get_error_code(void)
 {
-    return self.operation_status;
+    return self.operation_status_code;
 }
 
 static inline void i2c_send_start (void)
 {
     // Datasheet page 217 (v 1/2015)
-    TWCR |= (1 << TWINT) | (1 << TWEN) | (1 << TWSTA) | (1 << TWIE);
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTA) | (1 << TWIE);
 }
 
 static inline void i2c_write_byte (uint8_t byte)
@@ -117,21 +146,21 @@ static inline void i2c_write_byte (uint8_t byte)
     TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE);
 }
 
-i2c_operation_status_t i2c_restart (void)
+enum i2c_op_result_t i2c_restart (void)
 {
-    TWCR |= (1 << TWINT) | (1 << TWEN) | (1 << TWSTA);
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTA);
 
     //i2c_wait_for_complete();
     if (TW_STATUS != TW_REP_START)
     {
         return i2c_operation_error;
     }
-    return i2c_ok;
+    return i2c_operation_ok;
 }
 
 static inline void i2c_send_stop (void)
 {
-    TWCR |= (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
 }
 
 /*
@@ -141,19 +170,16 @@ ISR(TWI_vect)
 {
     switch (self.state)
     {
-        case idle:
-            // Error - cannot happen
-            self.operation_status = i2c_general_error;
-            break;
-
         case start:
             if (TW_STATUS == TW_START)
             {
+                self.state = send_sla_w;
                 i2c_write_byte(self.slave_address);
             }
             else
             {
-                self.operation_status = i2c_start_error;
+                *self.operation_result = i2c_operation_start_error;
+                self.operation_status_code = TW_STATUS;
             }
             // Stop shall not be sent
             return;
@@ -164,7 +190,7 @@ ISR(TWI_vect)
                 if (self.operation == write_op)
                 {
                     self.state = write_data;
-                    i2c_write_byte(self.data[self.handled_bytes++]);
+                    i2c_write_byte(self.buffer[self.handled_bytes++]);
                 }
                 else
                 {
@@ -175,28 +201,30 @@ ISR(TWI_vect)
             else
             {
                 // Stop and return error
-                self.operation_status = i2c_sla_error;
+                *self.operation_result = i2c_operation_sla_error;
+                self.operation_status_code = TW_STATUS;
             }
             break;
 
         case write_data:
             if (TW_STATUS == TW_MT_DATA_ACK)
             {
-                if (self.handled_bytes < self.data_len)
+                if (self.handled_bytes < self.buffer_length)
                 {
-                    i2c_write_byte(self.data[self.handled_bytes++]);
+                    i2c_write_byte(self.buffer[self.handled_bytes++]);
                     return;
                 }
                 else
                 {
                     // All bytes written -- stop and return ok
-                    self.operation_status = i2c_ok;
+                    *self.operation_result = i2c_operation_ok;
                 }
             }
             else
             {
                 // Stop and return error
-                self.operation_status = i2c_write_error;
+                *self.operation_result = i2c_operation_write_error;
+                self.operation_status_code = TW_STATUS;
             }
             break;
 
@@ -215,5 +243,6 @@ ISR(TWI_vect)
 //            }
             break;
     }
+
     i2c_send_stop();
 }
